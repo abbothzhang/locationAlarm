4de36dfc1d9835f5dc015364d6809f16e9752f9d
abbothzhang <zh513123@gmail.com>
2015-03-23 00:09:01 +0800

    添加LocationAlarm xcode工程

---
+++ LocationAlarm/LocationAlarm/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LocationAlarm
+//
+//  Created by 张辉 on 15-3-23.
+//  Copyright (c) 2015年 张辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LocationAlarm/LocationAlarm/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LocationAlarm
+//
+//  Created by 张辉 on 15-3-23.
+//  Copyright (c) 2015年 张辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
