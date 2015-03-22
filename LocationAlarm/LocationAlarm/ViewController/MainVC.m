6a25983277d7812b7fb3e6188caf5deda7ee40f1
abbothzhang <zh513123@gmail.com>
2015-03-23 00:50:41 +0800

    添加初步demo，工程还有问题，待调试

--- LocationAlarm/LocationAlarm/ViewController/MainVC.m
+++ LocationAlarm/LocationAlarm/ViewController/MainVC.m
@@ -7,8 +7,11 @@
 //
 
 #import "MainVC.h"
+#import "BMapKit.h"
 
-@interface MainVC ()
+@interface MainVC ()<BMKGeneralDelegate>
+
+@property (nonatomic,strong) BMKMapManager *mapManager;
 
 @end
 
@@ -17,22 +20,47 @@
 -(id)init{
     self = [super init];
     if (self) {
-        
+        [self initMap];
     }
     return self;
+}
+
+-(void)initMap{
+    _mapManager = [[BMKMapManager alloc]init];
+    // 如果要关注网络及授权验证事件，请设定generalDelegate参数
+    BOOL ret = [_mapManager start:@"OIXcyU6HLb634ZPbZynRv3DS" generalDelegate:self];
+    if (!ret) {
+        NSLog(@"manager start failed!");
+    }
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor greenColor];
+    BMKMapView* mapView = [[BMKMapView alloc]initWithFrame:CGRectMake(0, 0, 320, 480)];
+    self.view = mapView;
+}
+
+#pragma mark - BMKGeneralDelegate
+/**
+ *返回网络错误
+ *@param iError 错误号
+ */
+- (void)onGetNetworkState:(int)iError{
     
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+/**
+ *返回授权验证错误
+ *@param iError 错误号 : 为0时验证通过，具体参加BMKPermissionCheckResultCode
+ */
+- (void)onGetPermissionState:(int)iError{
+    
+}
+
+-(void)dealloc{
+    NSLog(@"MainVC dealloc");
 }
 
 @end
