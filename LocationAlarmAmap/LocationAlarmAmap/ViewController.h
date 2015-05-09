7b2e42e35ba958d3ce962deeeef53d7025e88a75
albert <penghui.zh@alibaba-inc.com>
2015-05-09 22:45:30 +0800

    添加位置闹钟高德地图demo

---
+++ LocationAlarmAmap/LocationAlarmAmap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocationAlarmAmap
+//
+//  Created by albert on 15/5/4.
+//  Copyright (c) 2015年 albert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LocationAlarmAmap/LocationAlarmAmap/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LocationAlarmAmap
+//
+//  Created by albert on 15/5/4.
+//  Copyright (c) 2015年 albert. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
